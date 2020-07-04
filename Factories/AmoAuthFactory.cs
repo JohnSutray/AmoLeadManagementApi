58206798a05a1d6e0eb921d998ec5e3ab8f54e41
Andrey Zobkov <a.zobkov@itransition.com>
2020-07-04 18:19:01 +0300

    Work done:  * Changed way of accepting amo credentials

--- Factories/AmoAuthFactory.cs
+++ Factories/AmoAuthFactory.cs
@@ -1,18 +1,29 @@
 ﻿using System;
 using AmoLeadManagementApi.Models;
-using AmoLeadManagementApi.Extensions.Configuration;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace AmoLeadManagementApi.Factories {
   public class AmoAuthFactory {
-    public static AmoAuth Create(IServiceProvider provider) {
-      var configuration = provider.GetRequiredService<IConfiguration>();
-      
+    public static AmoAuth Create(IServiceProvider provider) =>
+      provider.GetRequiredService<IHostEnvironment>().IsDevelopment()
+        ? AmoAuthFromConfiguration(provider.GetRequiredService<IConfiguration>())
+        : AmoAuthFromEnv;
+
+    private static AmoAuth AmoAuthFromEnv => new AmoAuth {
+      Domain = Environment.GetEnvironmentVariable("AMO_DOMAIN"),
+      UserHash = Environment.GetEnvironmentVariable("AMO_HASH"),
+      UserLogin = Environment.GetEnvironmentVariable("AMO_LOGIN")
+    };
+
+    private static AmoAuth AmoAuthFromConfiguration(IConfiguration configuration) {
+      var amo = configuration.GetSection("Amo");
+
       return new AmoAuth {
-        Domain = configuration.GetAmoDomain(),
-        UserHash = configuration.GetAmoHash(),
-        UserLogin = configuration.GetAmoLogin()
+        Domain = amo["Domain"],
+        UserHash = amo["Hash"],
+        UserLogin = amo["Login"]
       };
     }
   }
